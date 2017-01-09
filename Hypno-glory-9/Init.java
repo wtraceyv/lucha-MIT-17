81f8f12aa876cb5205d4d225db8ac2851eea759d
wtraceyv <wtraceyv@gmail.com>
2017-01-09 16:29:59 -0500

    Create Init.java

---
+++ Hypno-glory-9/Init.java
@@ -0,0 +1,12 @@
+package Hypno-glory-9; 
+
+import battlecode.common.*; 
+
+public class Init {
+
+/** 
+ * every robot should be extended from this class, 
+ * so that everyone gets these common abilities/traits. 
+ */ 
+
+}// end Init class 
