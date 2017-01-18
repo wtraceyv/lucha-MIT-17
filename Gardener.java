32e24c0c4a9ce55e3c848e465ae6741f3e69db6d
wtraceyv <wtraceyv@gmail.com>
2017-01-18 14:30:44 -0500

    Update Gardener.java

--- Gardener.java
+++ Gardener.java
@@ -13,16 +13,17 @@ import battlecode.common.*;
 
 public class Gardener extends RootBot{
 	
+	// make method to reset these, and find out when that 
+	// would be appropriate?
 	private static int soldiersDispensed = 0;
 	private static int treesPlanted = 0; 
+	private static int scoutsDispensed = 0; 
 	
 	public static void go(){
 		while (true){
 			try{ 
 				update(); 
-				tryPlantTree(); 
-				tryToWaterTrees(); 
-				Nav.tryMove(Nav.randomDirection()); 
+				execute();  
 				Clock.yield();
 			}
 			catch(Exception e){
@@ -31,26 +32,47 @@ public class Gardener extends RootBot{
 		}
 	}// end go()
 	
-	public static boolean tryPlantTree() throws GameActionException {
-		Direction newDir = Nav.randomDirection();
-		if (treesPlanted < 5 && closeTrees.length <= 5 && rc.canPlantTree(newDir)){
-			rc.plantTree(newDir);
-			treesPlanted++; 
-			System.out.println(me + " planted tree number " + treesPlanted); 
-			System.out.println("I am at: " + here);
-			return true; 
+	/**
+	 * how to plant a tree if needed 
+	 * -->use as boolean method? would it help? 
+	 * @return
+	 * @throws GameActionException
+	 */
+	public static void treeGroupPhase() throws GameActionException {
+		// setup directions to plant trees in 
+		Direction negativeDir = here.directionTo(centerInitEnemyArchons);
+		Direction[] plantingDirs = new Direction[7]; 
+		float offset = (float) 45; 
+		for (int i=7, j=0;i-->0;){
+			plantingDirs[j] = negativeDir.rotateLeftDegrees(offset); 
+			offset += 45; 
+			j++; 
 		}
-		return false; 
+		// plant the trees
+		for (Direction d : plantingDirs){
+			if (rc.canPlantTree(d)){
+				rc.plantTree(d);
+				treesPlanted++; 
+			}
+			else 
+				continue; 
+		}
 	}
 	
+	/**
+	 * attempt to water trees if appropriate 
+	 * 
+	 * @return
+	 * @throws GameActionException
+	 */
 	public static boolean tryToWaterTrees() throws GameActionException{
-		if (closeTrees.length>5){
+		if (closeTrees.length>0){
 			for(int i=closeTrees.length;i-->0;){
 				if(closeTrees[i].getHealth()<GameConstants.BULLET_TREE_MAX_HEALTH && rc.canWater(closeTrees[i].getID())){
 					rc.water(closeTrees[i].getID());
-					break; 
 				}
 			}
+			return true; 
 		}
 		return false; 
 	}
@@ -62,12 +84,25 @@ public class Gardener extends RootBot{
 	 * @throws GameActionException
 	 */
 	public static boolean dispenseBots() throws GameActionException{
-		Direction testDirection = Nav.randomDirection();
-		if (soldiersDispensed < 2 && rc.canBuildRobot(RobotType.SOLDIER, testDirection)){
-			rc.buildRobot(RobotType.SOLDIER, testDirection);
-			soldiersDispensed++; 
-			return true; 
+		return false;
+	}
+	
+	/**
+	 * main method to execute other than update() in go() 
+	 * --> keeps try/catch in Gardener loop less cluttered 
+	 * @throws GameActionException
+	 */
+	public static void execute() throws GameActionException {
+		Direction randTest = Nav.randomDirection(); 
+		if (scoutsDispensed<1&&rc.canBuildRobot(RobotType.SCOUT, randTest)){
+			rc.buildRobot(RobotType.SCOUT, randTest);
+			scoutsDispensed++;
 		}
-		return false; 
+		treeGroupPhase();  
+		tryToWaterTrees(); 
+		dispenseBots();
+		if (rc.getTeamBullets()>10000){
+			rc.donate(10000);
+		}
 	}
 }
