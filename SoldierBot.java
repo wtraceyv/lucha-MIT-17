942206d39372c48afa52b28d8b8f650eba8dd143
wtraceyv <wtraceyv@gmail.com>
2017-01-18 14:35:50 -0500

    Update SoldierBot.java

--- SoldierBot.java
+++ SoldierBot.java
@@ -5,17 +5,21 @@ import battlecode.common.*;
 /**
  * functions/micro are very primitive
  * these are pretty much just for testing purposes right now 
+ * --when to shake trees for bullets??
+ * --use a random three pick or incrementing int in radio broadcast 
+ * to keep track and use modulo to assign soldiers to different roles?
  * 
  * @author Wallisan
  */
 
 public class SoldierBot extends RootBot{
 	
-	public static void go(){
+	public static void go() throws GameActionException {
 		while(true){
 			update(); 
 			try{
-				Nav.goTo(target)goTo(initEnemyArchons); 
+				while (!here.isWithinDistance(centerInitEnemyArchons, (float)(me.sensorRadius*.75)))
+					Nav.goTo(centerInitEnemyArchons); 
 			}
 			catch(Exception e){
 				e.printStackTrace();
@@ -23,11 +27,34 @@ public class SoldierBot extends RootBot{
 		}
 	}// end go()
 	
-	public static boolean chargeBase(){
-		return false; 
+	public static void execute() throws GameActionException {
+		if (tryAttack())
+			return; 
+		else if (closeEnemies.length>0){
+			if (tryAttack())
+				return; 
+		}
+		else {
+			Nav.goTo(centerInitAllyArchons); 
+			return; 
+		}
 	}
 	
-	public static boolean tryAttack(){
+	/**
+	 * working on this right now -- helper methods will improve micro
+	 * @return true if attack is overall successful
+	 * @throws GameActionException
+	 */
+	public static boolean tryAttack() throws GameActionException {
+		if (closeEnemies.length>2){
+			for (RobotInfo bot : closeEnemies){
+				while (bot.getHealth()>0){
+					Nav.maintainDistance(bot, (float) (me.sensorRadius*.75)); 
+					Direction target = here.directionTo(bot.getLocation()); 
+					rc.fireSingleShot(target);
+				}
+			}
+		}
 		return false; 
 	}
 }// end SoldierBot class 
