c135c1f78e8e01dfe85536770b3cbbf710a47a12
wtraceyv <wtraceyv@gmail.com>
2017-01-29 17:10:32 -0500

    Update Comms.java

--- Comms.java
+++ Comms.java
@@ -13,12 +13,14 @@ public class Comms extends RootBot {
 	
 	// declare some channels for various purposes 
 	// let's just assume there can only be 3 archons/team max 
-	static int FIRST_FOUND_ARCHON_CHANNEL = 478;
-	static int SECOND_FOUND_ARCHON_CHANNEL = 657; 
-	static int THIRD_FOUND_ARCHON_CHANNEL = 245; 
-	static int ARCHON_IN_DANGER_ONE = 223; 
-	static int ARCHON_IN_DANGER_TWO = 212; 
-	static int ARCHON_IN_DANGER_THREE = 226; 
+	static int FIRST_FOUND_ARCHON_CHANNEL = 42;
+	static int SECOND_FOUND_ARCHON_CHANNEL = 75; 
+	static int THIRD_FOUND_ARCHON_CHANNEL = 85; 
+	static int ARCHON_IN_DANGER_ONE = 145; 
+	static int ARCHON_IN_DANGER_TWO = 168; 
+	static int ARCHON_IN_DANGER_THREE = 112; 
+	static int ENEMY_MOB_SMALL = 1649; 
+	static int ENEMY_MOB_LARGE = 1947; 
 	
 	/**
 	 * broadcast the location and ID of a found archon for other bots, 
@@ -33,8 +35,16 @@ public class Comms extends RootBot {
 		int xCoord = (int) found.x; 
 		int yCoord = (int) found.y; 
 		rc.broadcast(index, xCoord); 
-		rc.broadcast(index+1, yCoord); 
-		rc.broadcast(index+2, bot.getID());
+		rc.broadcast((index+1), yCoord); 
+		rc.broadcast((index+2), bot.getID());
+	}
+	
+	public static void broadcastArchonDanger(MapLocation spot, int index) throws GameActionException {
+		int xCoord = (int) spot.x; 
+		int yCoord = (int) spot.y; 
+		rc.broadcast(index, xCoord); 
+		rc.broadcast(index+1, yCoord);
+		rc.broadcast(index+2, rc.getID());
 	}
 	
 	/**
@@ -46,14 +56,24 @@ public class Comms extends RootBot {
 	public static MapLocation[] readArchonLocations() throws GameActionException{
 		MapLocation[] end = new MapLocation[3]; 
 		int[] indices = new int[] {FIRST_FOUND_ARCHON_CHANNEL, 
-				           SECOND_FOUND_ARCHON_CHANNEL, 
-				           THIRD_FOUND_ARCHON_CHANNEL};
-		for (int i = 0; i++<=2;){
+				                   SECOND_FOUND_ARCHON_CHANNEL, 
+				                   THIRD_FOUND_ARCHON_CHANNEL};
+		for (int i = 0; i<=2; i++){
 			// fill each MapLocation with coords from the channels, even if they are still 0, 0
 			end[i] = new MapLocation(rc.readBroadcast(indices[i]),
-					         rc.readBroadcast(indices[i])+1); 
+					                 rc.readBroadcast(indices[i]+1)); 
 		}
 		return end; 
+	}
+	
+	public static MapLocation checkArchonsInDanger() throws GameActionException {
+		int[] IDs = {ARCHON_IN_DANGER_ONE+2,ARCHON_IN_DANGER_TWO+2,ARCHON_IN_DANGER_THREE+2}; 
+		for (int ID : IDs){
+			if (ID!=0){
+				return new MapLocation(rc.readBroadcast(ID-2),rc.readBroadcast(ID-1)); 
+			}
+		}
+		return null; 
 	}
 	
 	/**
@@ -67,7 +87,7 @@ public class Comms extends RootBot {
 	 */
 	public static MapLocation readLocation(int startIndex) throws GameActionException {
 		MapLocation read = new MapLocation(rc.readBroadcast(startIndex),
-				                   rc.readBroadcast(startIndex)+1); 
+										   rc.readBroadcast(startIndex+1)); 
 		return read; 
 	}
 	
@@ -79,12 +99,25 @@ public class Comms extends RootBot {
 	 * @throws GameActionException
 	 */
 	public static boolean archonAlreadySeen(RobotInfo bot) throws GameActionException {
-		if (bot.getID()==rc.readBroadcast(FIRST_FOUND_ARCHON_CHANNEL+2)
-	          ||bot.getID()==rc.readBroadcast(SECOND_FOUND_ARCHON_CHANNEL+2)
-	          ||bot.getID()==rc.readBroadcast(THIRD_FOUND_ARCHON_CHANNEL+2)) {
+		int ID = bot.getID(); 
+		if (ID==rc.readBroadcast(FIRST_FOUND_ARCHON_CHANNEL+2))
 			return true; 
-		}
-		return false; 
+		if (ID==rc.readBroadcast(SECOND_FOUND_ARCHON_CHANNEL+2))
+			return true;
+		if (ID==rc.readBroadcast(THIRD_FOUND_ARCHON_CHANNEL+2))
+			return true; 
+		return false;  
+	}
+	
+	public static boolean alreadyCalledBackup() throws GameActionException {
+		int ID = rc.getID(); 
+		if (ID==rc.readBroadcast(ARCHON_IN_DANGER_ONE+2))
+			return true; 
+		if (ID==rc.readBroadcast(ARCHON_IN_DANGER_TWO+2))
+			return true;
+		if (ID==rc.readBroadcast(ARCHON_IN_DANGER_THREE+2))
+			return true; 
+		return false;  
 	}
 	
 	/**
@@ -92,12 +125,20 @@ public class Comms extends RootBot {
 	 * @throws GameActionException
 	 */
 	public static int nextArchonIndexFree() throws GameActionException {
-		if (FIRST_FOUND_ARCHON_CHANNEL==0)
+		if (MapMath.floatEquals(rc.readBroadcast(FIRST_FOUND_ARCHON_CHANNEL), 0))
 			return FIRST_FOUND_ARCHON_CHANNEL; 
-		else if (SECOND_FOUND_ARCHON_CHANNEL==0)
+		else if (MapMath.floatEquals(rc.readBroadcast(SECOND_FOUND_ARCHON_CHANNEL), 0))
 			return SECOND_FOUND_ARCHON_CHANNEL; 
 		else 
 			return THIRD_FOUND_ARCHON_CHANNEL; 
+	}
+	
+	public static int nextDangerIndexFree() throws GameActionException {
+		if (rc.readBroadcast(ARCHON_IN_DANGER_ONE)==0)
+			return ARCHON_IN_DANGER_ONE; 
+		else if (rc.readBroadcast(ARCHON_IN_DANGER_TWO)==0)
+			return ARCHON_IN_DANGER_TWO; 
+		return ARCHON_IN_DANGER_THREE; 
 	}
 	
 	/**
@@ -106,8 +147,12 @@ public class Comms extends RootBot {
 	 * @param bot
 	 * @throws GameActionException
 	 */
-	public static void storeNewArchonLocation(MapLocation found, RobotInfo bot) throws GameActionException {
+	public static void broadcastNewArchonLocation(MapLocation found, RobotInfo bot) throws GameActionException {
 		broadcastArchonLocation(found, nextArchonIndexFree(), bot);
+	}
+	
+	public static void broadcastNewDangerCall(MapLocation danger) throws GameActionException {
+		broadcastArchonDanger(here, nextDangerIndexFree());
 	}
 	
 	/**
@@ -118,15 +163,84 @@ public class Comms extends RootBot {
 	 * @param bot archon found so we can use its ID in decision making
 	 */
 	public static void updateArchonLocation(MapLocation found, RobotInfo bot) throws GameActionException {
-		for (int possibleIndex : new int[] {FIRST_FOUND_ARCHON_CHANNEL+2,
-				                    SECOND_FOUND_ARCHON_CHANNEL+2,
-				                    THIRD_FOUND_ARCHON_CHANNEL+2}){
+		int[] IDs = new int[] {(FIRST_FOUND_ARCHON_CHANNEL+2),
+                			   (SECOND_FOUND_ARCHON_CHANNEL+2),
+                               (THIRD_FOUND_ARCHON_CHANNEL+2)};
+		for (int possibleIndex : IDs){
 			if (rc.readBroadcast(possibleIndex)==bot.getID()){
-				broadcastArchonLocation(found, possibleIndex, bot);
-			} else {
+				broadcastArchonLocation(found, (possibleIndex-2), bot);
 				return; 
 			}
 		}
+	}
+	
+	public static void updateArchonDanger(MapLocation found) throws GameActionException {
+		int[] IDs = new int[] {(ARCHON_IN_DANGER_ONE+2),
+                			   (ARCHON_IN_DANGER_TWO+2),
+                               (ARCHON_IN_DANGER_THREE+2)};
+		for (int possibleIndex : IDs){
+			if (rc.readBroadcast(possibleIndex)==rc.getID()){
+				broadcastArchonDanger(found, (possibleIndex-2));
+				return; 
+			}
+		}
+	}
+	
+	/**
+	 * I found an archon! --> 
+	 * -- decide whether I've seen this archon already; 
+	 * if yes, update its location 
+	 * if no, broadcast its info in next available index (mostly Comms work)
+	 */
+	public static void screamArchon(RobotInfo found, MapLocation whereFound) throws GameActionException {
+		if (archonAlreadySeen(found)){
+			updateArchonLocation(whereFound, found);
+			return; 
+		} else {
+			broadcastNewArchonLocation(whereFound, found);
+		}
+	}// end screamArchon()
+	
+	public static void callBackup(MapLocation danger) throws GameActionException {
+		if (Comms.alreadyCalledBackup()){
+			updateArchonDanger(danger); 
+			return; 
+		} else {
+			broadcastNewDangerCall(danger);
+		}
+	}// end callBackup() 
+	
+	public static void clear(int safeBotID) throws GameActionException {
+		int indexToClear = -1; 
+		int[] possibleIDs = {ARCHON_IN_DANGER_ONE+2, 
+							 ARCHON_IN_DANGER_TWO+2, 
+							 ARCHON_IN_DANGER_THREE+2}; 
+		for (int safeChannel : possibleIDs){
+			if (rc.readBroadcast(safeChannel)==rc.getID()){
+				indexToClear = safeChannel-2; 
+			}
+		}
+		rc.broadcast(indexToClear, 0);
+		rc.broadcast(indexToClear+1, 0);
+		rc.broadcast(indexToClear+2, 0);
+	}// end clear() 
+	
+//	public static boolean archonInDanger(){
+//		int[] 
+//		return false; 
+//	}
+	
+	// These were for debugging purposes 
+	public static void printArchonLocs() throws GameActionException {
+		System.out.println("First store: " + rc.readBroadcast(FIRST_FOUND_ARCHON_CHANNEL) + " " + rc.readBroadcast(FIRST_FOUND_ARCHON_CHANNEL+1));
+		System.out.println("Second store: " + rc.readBroadcast(SECOND_FOUND_ARCHON_CHANNEL) + " " + rc.readBroadcast(SECOND_FOUND_ARCHON_CHANNEL+1));
+		System.out.println("Third store: " + rc.readBroadcast(THIRD_FOUND_ARCHON_CHANNEL) + " " + rc.readBroadcast(THIRD_FOUND_ARCHON_CHANNEL+1));
+	}
+	
+	public static void printArchonIDs() throws GameActionException {
+		System.out.println("First ID: " + rc.readBroadcast(FIRST_FOUND_ARCHON_CHANNEL+2));
+		System.out.println("Second ID: " + rc.readBroadcast(SECOND_FOUND_ARCHON_CHANNEL+2));
+		System.out.println("Third ID: " + rc.readBroadcast(THIRD_FOUND_ARCHON_CHANNEL+2));
 	}
 	
 }// end Comms class 
